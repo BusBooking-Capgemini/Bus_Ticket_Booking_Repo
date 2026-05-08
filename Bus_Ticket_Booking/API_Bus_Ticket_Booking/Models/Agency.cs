0c91f35ad8d144e2a630e56b168b8b6b99e0c2ec
Mahesh <115633814+Calcifer077@users.noreply.github.com>
2026-05-08 10:24:39 +0530

    Done scaffolding

---
+++ Bus_Ticket_Booking/API_Bus_Ticket_Booking/Models/Agency.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API_Bus_Ticket_Booking.Models;
+
+public partial class Agency
+{
+    public int AgencyId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string ContactPersonName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Phone { get; set; } = null!;
+
+    public virtual ICollection<AgencyOffice> AgencyOffices { get; set; } = new List<AgencyOffice>();
+}
