99fa49f534946f8a49acc9e121247a227f817fc6
info-nikhil <info.nikhil001@gmail.com>
2026-05-11 10:56:51 +0530

    Updated Agency and Office Validations

---
+++ Bus_Ticket_Booking/API_Bus_Ticket_Booking/Validators/Agency/AgencyCreateDtoValidator.cs
@@ -0,0 +1,35 @@
+﻿using API_Bus_Ticket_Booking.DTOs.Agency;
+using FluentValidation;
+
+namespace API_Bus_Ticket_Booking.Validators.Agency
+{
+    public class AgencyCreateDtoValidator : AbstractValidator<AgencyRequestDto>
+    {
+        public AgencyCreateDtoValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage("Agency name is required")
+                .MaximumLength(255);
+
+            RuleFor(x => x.ContactPersonName)
+                .NotEmpty()
+                .WithMessage("Contact person name is required")
+                .MaximumLength(30);
+
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .WithMessage("Email is required")
+                .EmailAddress()
+                .WithMessage("Invalid email format")
+                .MaximumLength(255);
+
+            RuleFor(x => x.Phone)
+                .NotEmpty()
+                .WithMessage("Phone number is required")
+                .Matches(@"^[0-9]{10,15}$")
+                .WithMessage(
+                    "Phone number must contain only digits");
+        }
+    }
+}
---
+++ Bus_Ticket_Booking/API_Bus_Ticket_Booking/Validators/Office/OfficeCreateDtoValidator.cs
@@ -0,0 +1,39 @@
+﻿using API_Bus_Ticket_Booking.DTOs.Office;
+using FluentValidation;
+
+namespace API_Bus_Ticket_Booking.Validators.Office
+{
+    public class OfficeCreateDtoValidator : AbstractValidator<OfficeRequestDto>
+    {
+        public OfficeCreateDtoValidator()
+        {
+            RuleFor(x => x.AgencyId)
+                .GreaterThan(0)
+                .WithMessage("AgencyId is required");
+
+            RuleFor(x => x.OfficeMail)
+                .NotEmpty()
+                .WithMessage("Office email is required")
+                .EmailAddress()
+                .WithMessage("Invalid office email")
+                .MaximumLength(100);
+
+            RuleFor(x => x.OfficeContactPersonName)
+                .NotEmpty()
+                .WithMessage(
+                    "Office contact person name is required")
+                .MaximumLength(50);
+
+            RuleFor(x => x.OfficeContactNumber)
+                .NotEmpty()
+                .WithMessage("Office contact number is required")
+                .Matches(@"^[0-9]{10}$")
+                .WithMessage(
+                    "Office contact number must be 10 digits");
+
+            RuleFor(x => x.OfficeAddressId)
+                .GreaterThan(0)
+                .WithMessage("OfficeAddressId is required");
+        }
+    }
+}
