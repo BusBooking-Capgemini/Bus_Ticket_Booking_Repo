a6348daafd95663b402f0e994499719d3874ae8f
info-nikhil <info.nikhil001@gmail.com>
2026-05-12 15:57:13 +0530

    Office Controller

--- Bus_Ticket_Booking/API_Bus_Ticket_Booking/Controllers/OfficeController.cs
+++ Bus_Ticket_Booking/API_Bus_Ticket_Booking/Controllers/OfficeController.cs
@@ -1,11 +1,13 @@
 ﻿using API_Bus_Ticket_Booking.DTOs.Office;
 using API_Bus_Ticket_Booking.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API_Bus_Ticket_Booking.Controllers
 {
     [Route("api/offices")]
     [ApiController]
+    [Authorize(Roles = "Office,Agency")]
     public class OfficeController : ControllerBase
     {
         private readonly IOfficeService _service;
@@ -17,6 +19,7 @@ namespace API_Bus_Ticket_Booking.Controllers
 
         // 1. GET all offices
         [HttpGet]
+        [Authorize(Roles = "Agency")]
         public async Task<IActionResult> GetAll()
         {
             var result = await _service.GetAllAsync();
@@ -32,6 +35,7 @@ namespace API_Bus_Ticket_Booking.Controllers
 
         // 2. GET office by id
         [HttpGet("{id}")]
+        [Authorize(Roles = "Office,Agency")]
         public async Task<IActionResult> GetById(int id)
         {
             var result = await _service.GetByIdAsync(id);
@@ -46,6 +50,7 @@ namespace API_Bus_Ticket_Booking.Controllers
 
         // 3. POST create office
         [HttpPost]
+        [Authorize(Roles = "Agency")]
         public async Task<IActionResult> Create([FromBody] OfficeRequestDto dto)
         {
             var result = await _service.CreateAsync(dto);
@@ -63,6 +68,7 @@ namespace API_Bus_Ticket_Booking.Controllers
 
         // 4. PUT update office
         [HttpPut("{id}")]
+        [Authorize(Roles = "Office,Agency")]
         public async Task<IActionResult> Update(int id, [FromBody] OfficeRequestDto dto)
         {
             await _service.UpdateAsync(id, dto);
@@ -76,6 +82,7 @@ namespace API_Bus_Ticket_Booking.Controllers
 
         // 5. DELETE office
         [HttpDelete("{id}")]
+        [Authorize(Roles = "Agency")]
         public async Task<IActionResult> Delete(int id)
         {
             await _service.DeleteAsync(id);
@@ -89,6 +96,7 @@ namespace API_Bus_Ticket_Booking.Controllers
 
         // 6. GET office summary
         [HttpGet("{id}/summary")]
+        [Authorize(Roles = "Office,Agency")]
         public async Task<IActionResult> GetSummary(int id)
         {
             var result = await _service.GetSummaryAsync(id);
@@ -103,6 +111,7 @@ namespace API_Bus_Ticket_Booking.Controllers
 
         // 7. GET office buses
         [HttpGet("{id}/buses")]
+        [Authorize(Roles = "Office,Agency")]
         public async Task<IActionResult> GetBuses(int id)
         {
             var result = await _service.GetBusesAsync(id);
@@ -118,6 +127,7 @@ namespace API_Bus_Ticket_Booking.Controllers
 
         // 8. GET office drivers
         [HttpGet("{id}/drivers")]
+        [Authorize(Roles = "Office,Agency")]
         public async Task<IActionResult> GetDrivers(int id)
         {
             var result = await _service.GetDriversAsync(id);
@@ -133,6 +143,7 @@ namespace API_Bus_Ticket_Booking.Controllers
 
         // 9. GET office trips
         [HttpGet("{id}/trips")]
+        [Authorize(Roles = "Office,Agency")]
         public async Task<IActionResult> GetTrips(int id)
         {
             var result = await _service.GetTripsAsync(id);
@@ -148,6 +159,7 @@ namespace API_Bus_Ticket_Booking.Controllers
 
         // 10. GET office bookings
         [HttpGet("{id}/bookings")]
+        [Authorize(Roles = "Office,Agency")]
         public async Task<IActionResult> GetBookings(int id)
         {
             var result = await _service.GetBookingsAsync(id);
@@ -163,6 +175,7 @@ namespace API_Bus_Ticket_Booking.Controllers
 
         // 11. GET office payments
         [HttpGet("{id}/payments")]
+        [Authorize(Roles = "Office,Agency")]
         public async Task<IActionResult> GetPayments(int id)
         {
             var result = await _service.GetPaymentsAsync(id);
