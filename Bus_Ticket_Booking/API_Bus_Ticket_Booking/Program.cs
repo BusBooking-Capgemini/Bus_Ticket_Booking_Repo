0b3d62a9bfedcab7d70290e90c7ef10eb51fb04a
info-nikhil <info.nikhil001@gmail.com>
2026-05-13 10:24:17 +0530

    Fixed DB String

--- Bus_Ticket_Booking/API_Bus_Ticket_Booking/Program.cs
+++ Bus_Ticket_Booking/API_Bus_Ticket_Booking/Program.cs
@@ -32,8 +32,16 @@ public class Program
         var jwtAudience = Environment.GetEnvironmentVariable("JWT_AUDIENCE");
 
         // Database
+        //builder.Services.AddDbContext<BusTicketBookingContext>(options =>
+        //    options.UseSqlServer(builder.Configuration.GetConnectionString("connectionString"))
+        //);
+
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            throw new Exception("DB_CONNECTION_STRING is missing");
+        }
         builder.Services.AddDbContext<BusTicketBookingContext>(options =>
-            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+            options.UseSqlServer(connectionString)
         );
 
         builder.Services.AddControllers();
