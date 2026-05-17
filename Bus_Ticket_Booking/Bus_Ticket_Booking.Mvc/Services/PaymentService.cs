847564e502081c0cb62eb180d388513b60b00aea
Mahesh <115633814+Calcifer077@users.noreply.github.com>
2026-05-17 17:27:15 +0530

    Fix: Trip Date
    Now user can't book outdated or expired trips

--- Bus_Ticket_Booking/Bus_Ticket_Booking.Mvc/Services/PaymentService.cs
+++ Bus_Ticket_Booking/Bus_Ticket_Booking.Mvc/Services/PaymentService.cs
@@ -1,41 +1,32 @@
-﻿using Bus_Ticket_Booking.Mvc.Helpers;
+﻿using System.Text;
+using Bus_Ticket_Booking.Mvc.Helpers;
 using Bus_Ticket_Booking.Mvc.Models.Common;
 using Bus_Ticket_Booking.Mvc.Services.Interfaces;
 using Bus_Ticket_Booking.Mvc.ViewModels.Payment;
 using Newtonsoft.Json;
-using System.Text;
 
 namespace Bus_Ticket_Booking.Mvc.Services
 {
-    public class PaymentService
-        : IPaymentService
+    public class PaymentService : IPaymentService
     {
-        private readonly HttpClient
-            _httpClient;
+        private readonly HttpClient _httpClient;
 
-        private readonly IConfiguration
-            _configuration;
+        private readonly IConfiguration _configuration;
 
         private readonly string _baseUrl;
 
-        public PaymentService(
-            HttpClient httpClient,
-            IConfiguration configuration)
+        public PaymentService(HttpClient httpClient, IConfiguration configuration)
         {
             _httpClient = httpClient;
 
             _configuration = configuration;
 
-            _baseUrl =
-                _configuration["ApiSettings:BaseUrl"]
-                ?? "";
+            _baseUrl = _configuration["ApiSettings:BaseUrl"] ?? "";
         }
 
         // SIMULATED PAYMENT
 
-        public async Task<bool>
-            ProcessPaymentAsync(
-                PaymentViewModel model)
+        public async Task<bool> ProcessPaymentAsync(PaymentViewModel model)
         {
             await Task.Delay(1500);
 
@@ -44,91 +35,52 @@ namespace Bus_Ticket_Booking.Mvc.Services
 
         // CREATE PAYMENT
 
-        public async Task<bool>
-            CreatePaymentAsync(
-                int bookingId,
-                decimal amount,
-                string token)
+        public async Task<bool> CreatePaymentAsync(int bookingId, decimal amount, string token)
         {
-            var customerId =
-                JwtHelper.GetClaim(
-                    token,
-                    "CustomerId");
+            var customerId = JwtHelper.GetClaim(token, "CustomerId");
 
             var request = new
             {
                 BookingId = bookingId,
 
-                CustomerId =
-                    Convert.ToInt32(
-                        customerId),
+                CustomerId = Convert.ToInt32(customerId),
 
-                Amount = amount
+                Amount = amount,
             };
 
-            var json =
-                JsonConvert.SerializeObject(
-                    request);
+            var json = JsonConvert.SerializeObject(request);
 
-            var content =
-                new StringContent(
-                    json,
-                    Encoding.UTF8,
-                    "application/json");
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
 
-            _httpClient.DefaultRequestHeaders
-                .Authorization =
-                new System.Net.Http.Headers
-                .AuthenticationHeaderValue(
-                    "Bearer",
-                    token);
+            _httpClient.DefaultRequestHeaders.Authorization =
+                new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
 
-            var response =
-                await _httpClient.PostAsync(
-                    $"{_baseUrl}payment/create",
-                    content);
+            var response = await _httpClient.PostAsync($"{_baseUrl}payment/create", content);
 
-            return response
-                .IsSuccessStatusCode;
+            return response.IsSuccessStatusCode;
         }
 
         // MY PAYMENTS
 
-        public async Task<
-            List<PaymentListViewModel>>
-            GetMyPaymentsAsync(
-                string token)
+        public async Task<List<PaymentListViewModel>> GetMyPaymentsAsync(string token)
         {
-            _httpClient.DefaultRequestHeaders
-                .Authorization =
-                new System.Net.Http.Headers
-                .AuthenticationHeaderValue(
-                    "Bearer",
-                    token);
+            _httpClient.DefaultRequestHeaders.Authorization =
+                new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
 
-            var response =
-                await _httpClient.GetAsync(
-                    $"{_baseUrl}payment/my-payments");
+            var response = await _httpClient.GetAsync($"{_baseUrl}payment/my-payments");
 
             if (!response.IsSuccessStatusCode)
             {
-                return new List<
-                    PaymentListViewModel>();
+                return new List<PaymentListViewModel>();
             }
 
-            var content =
-                await response.Content
-                    .ReadAsStringAsync();
+            var content = await response.Content.ReadAsStringAsync();
 
-            var result =
-                JsonConvert.DeserializeObject<
-                    ApiResponse<
-                        List<PaymentListViewModel>>>
-                    (content);
+            var result = JsonConvert.DeserializeObject<ApiResponse<List<PaymentListViewModel>>>(
+                content
+            );
 
-            return result?.Data
-                   ?? new List<
-                        PaymentListViewModel>();
+            return result?.Data ?? new List<PaymentListViewModel>();
         }
     }
 }
